7046537fd1daf29063da51f3868e7dbf4ea166a0
jinshuaiwang <wangjinshuai.jd.com>
2019-05-22 22:50:21 +0800

    lru

--- src/main/java/com/test/algorithm/client/AlgorithmClient.java
+++ src/main/java/com/test/algorithm/client/AlgorithmClient.java
@@ -13,14 +13,14 @@ public class AlgorithmClient {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
 
-//        list.add("1");
-//        list.add("3");
-//        list.add("5");
-//        list.add("7");
-//        list.add("9");
-//        list.add("11");
-//        list.add("14");
-//        list.add("19");
+        list.add("1");
+        list.add("3");
+        list.add("5");
+        list.add("7");
+        list.add("9");
+        list.add("11");
+        list.add("14");
+        list.add("19");
         list.add("25");
         list.add("30");
         list.add("39");
--- src/main/java/com/test/algorithm/linkedlist/LinkedList.java
+++ src/main/java/com/test/algorithm/linkedlist/LinkedList.java
@@ -166,5 +166,21 @@ public class LinkedList {
             this.item = item;
             this.next = next;
         }
+
+        public E getItem() {
+            return item;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setItem(E item) {
+            this.item = item;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
     }
 }
