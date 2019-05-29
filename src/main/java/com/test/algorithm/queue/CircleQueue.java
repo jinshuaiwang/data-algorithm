a1c1bc8109a733ad104e534b02c8019a4c281dfa
wangjinshuai <wangjinshuai@jd.com>
2019-05-29 18:38:38 +0800

    circle queue

---
+++ src/main/java/com/test/algorithm/queue/CircleQueue.java
@@ -0,0 +1,25 @@
+package com.test.algorithm.queue;
+
+/**
+ * Description:
+ * Author: wangjinshuai
+ * Date: 2019-05-29 18:17
+ * Email: wangjinshuai@jd.com
+ */
+public class CircleQueue {
+
+    /**
+     * 队列元素
+     */
+    private String[] item;
+
+    /**
+     * 队列头
+     */
+    private int head = 0;
+
+    /**
+     * 队列尾
+     */
+    private int tail = 0;
+}
